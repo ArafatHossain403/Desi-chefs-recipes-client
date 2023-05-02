97c7ac0f31f5be7754478ea61050e6adf2ddfd51
Arafat Hossain <arafatnubt@gmail.com>
2023-05-02 23:06:30 +0600

    server data restore

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from '../pages/Shared/Header/Header';
 import Footer from '../pages/Shared/Footer/Footer';
-import Banner from '../pages/Shared/Bannner/Banner';
+import Banner from '../pages/Shared/Banner/Banner';
+import Chefs from '../pages/Home/Chefs/Chefs';
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
             <Banner></Banner>
+            <Chefs></Chefs>
 
 
             <Footer></Footer>
---
+++ src/pages/Home/Chefs/Chefs.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Chefs = () => {
+    const [chefs, setChefs]= useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/chefs')
+        .then(res => res.json)
+        .then(data => console.log(data))
+        .catch(error => console.error(error))
+    },[])
+    return (
+        <div>
+            <Container>
+            <h4 className='text-center'> Our All Bangladeshi Chefs And Their Recipes</h4>
+                {
+                    chefs.map(chef => <p 
+                    key={chef.id}
+                    >
+                        <Link>{chef.name}</Link>
+
+                    </p>)
+                }
+
+
+
+
+                </Container>
+            
+        </div>
+    );
+};
+
+export default Chefs;
