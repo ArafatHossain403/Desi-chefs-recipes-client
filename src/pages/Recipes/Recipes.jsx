7fadcdb44ac90592ade7e27045c275a185dde809
Arafat Hossain <arafatnubt@gmail.com>
2023-05-04 05:13:40 +0600

    rating added

--- src/pages/Recipes/Recipes.jsx
+++ src/pages/Recipes/Recipes.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import { BsBookmark} from "react-icons/bs";
+import { FaRegStar, FaStar } from 'react-icons/fa';
+import Rating from 'react-rating';
 
 const Recipes = ({recipe}) => {
-    const {recipe_name, ingredients, instructions}= recipe;
+    const {recipe_name, ingredients, instructions, rating}= recipe;
     return (
         <div className='mb-4'>
              <Card border="primary" style={{ width: 'auto' }}>
@@ -13,6 +16,20 @@ const Recipes = ({recipe}) => {
           <Card.Text>{ingredients}</Card.Text>
           <Card.Header>Instructions:</Card.Header>
           <Card.Text>{instructions}</Card.Text>
+          <div className='d-flex'>
+            <div className='flex-grow-1'>
+                <Rating
+                placeholderRating={rating}
+                emptySymbol={<FaRegStar />}
+                readonly
+                placeholderSymbol={<FaStar className='text-warning' />}
+                fullSymbol={<FaStar />}></Rating>
+                <span className='text-muted pl-2'>{rating}</span>
+            </div>
+
+            <BsBookmark/>
+            </div>
+
           </Card.Body>
           </Card>
         </div>
