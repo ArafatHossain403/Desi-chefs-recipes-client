7a536a427f18525349848d3781be52a66bf0888a
Arafat Hossain <arafatnubt@gmail.com>
2023-05-04 20:35:31 +0600

    Blog written

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -6,12 +6,7 @@ import Footer from '../Shared/Footer/Footer';
 const Error = () => {
     return (
         <div>
-            <Header></Header>
             
-            <Container className='w-50 mx-auto'>
-                <img height={500} src="https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg" alt="" />
-            </Container>
-            <Footer></Footer>
             
         </div>
     );
